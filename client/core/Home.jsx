c92c6e78e10d7c53f71cd6492a17cc57f8beb722
wilson <caozhaolong@webull.com>
2024-08-08 14:10:10 -0400

    wilson fix: bugs

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -7,10 +7,15 @@ import Typography from '@material-ui/core/Typography';
 import unicornbikeImg from './../assets/images/group-logo.png';
 
 const useStyles = makeStyles(theme => ({
+ root: {
+  backgroundColor: 'black',
+
+ },
  card: {
  maxWidth: 600,
  margin: 'auto',
  marginTop: theme.spacing(5),
+  backgroundColor: 'black',
  },
  title: {
  padding: theme.spacing(3, 2.5, 2),
@@ -20,20 +25,59 @@ const useStyles = makeStyles(theme => ({
  minHeight: 400,
  },
 }));
-export default function Home(){ 
+export default function Home(){
 const classes = useStyles()
 return (
 <Card className={classes.card}>
 
- <Typography variant="h6" className={classes.title}>Home Page</Typography>
+ {/*<Typography variant="h6" className={classes.title}>Home Page</Typography>*/}
 <CardMedia className={classes.media}
 image={unicornbikeImg} title="Unicorn Bicycle"/>
-<CardContent>
-<Typography variant="body2" component="p"> 
-Welcome to the Product List home page.
-</Typography> 
-</CardContent>
-</Card> 
+{/*<CardContent>*/}
+{/*<Typography variant="body2" component="p">*/}
+{/*Welcome to the Product List home page.*/}
+{/*</Typography>*/}
+{/*</CardContent>*/}
+</Card>
 )
 }
 
+
+
+// import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Card from '@material-ui/core/Card';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import unicornbikeImg from './../assets/images/group-logo.png';
+//
+// const useStyles = makeStyles(theme => ({
+//  root: {
+//   backgroundColor: 'black',
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//  },
+//  card: {
+//   backgroundColor: 'black',
+//  },
+//  media: {
+//   height: '80vh',
+//   width: 'auto',
+//  },
+// }));
+//
+// export default function Home() {
+//  const classes = useStyles();
+//  return (
+//      <div className={classes.root}>
+//       <Card className={classes.card}>
+//        <CardMedia
+//            className={classes.media}
+//            image={unicornbikeImg}
+//            title="Unicorn Bicycle"
+//        />
+//       </Card>
+//      </div>
+//  );
+// }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import MainRouter from '../MainRouter';
 import theme from '../theme';
+import './App.css'; // 引入全局 CSS 文件
+
 //import { hot } from 'react-hot-loader'
 const App = () => {
  return (
